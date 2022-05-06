b465bde7925bf1a9ea22c9b2c1dc20bd34edb68c
James Etchells <etch.rush@gmail.com>
2022-05-06 15:14:03 -0700

    Add filePath variable

--- index.js
+++ index.js
@@ -3,16 +3,42 @@ import path from 'path';
 import fs from 'fs';
 
 const server = createServer((req, res) => {
-    if (req.url === "/") {
-        fs.readFile(
-            path.join('./', 'public', 'index.html'),
-            (err, content) => {
-                if (err) throw err;
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(content);
+    // if (req.url === "/") {
+    //     fs.readFile(
+    //         path.join('./', 'public', 'index.html'),
+    //         (err, content) => {
+    //             if (err) throw err;
+    //             res.writeHead(200, { 'Content-Type': 'text/html' });
+    //             res.end(content);
+    //         }
+    //     )
+    // }
+
+    let filePath = path.join(
+        './',
+        'public',
+        req.url === '/' ? 'index.html' : req.url
+    )
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                // Page not found
+                fs.readFile(path.join('./', 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, { 'Content-Type': 'text/hmtl' });
+                    res.end(content, 'utf-8');
+                })
+            } else {
+                // Some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`)
             }
-        )
-    }
+        } else {
+            // Success
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf-8');
+        }
+    })
 });
 
 const PORT = process.env.PORT || 5000;
