82cc4e9f216abcbd74b67ec3e2e79a2a976eef14
James Etchells <etch.rush@gmail.com>
2022-05-06 14:47:37 -0700

    Add index.html

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ import fs from 'fs';
 
 const server = createServer((req, res) => {
     if (req.url === "/") {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end('<h1>HomePage</h1>');
+        fs.readFile(
+            path.join('./', 'public', 'index.html'),
+            (err, content) => {
+                if (err) throw err;
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content);
+            }
+        )
     }
 });
 
 const PORT = process.env.PORT || 5000;
+
 
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
