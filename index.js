6914940d695515e89b48b2704ab81859018f95be
James Etchells <etch.rush@gmail.com>
2022-05-09 11:22:19 -0700

    Add assets folder and background image

--- index.js
+++ index.js
@@ -3,17 +3,7 @@ import path from 'path';
 import fs from 'fs';
 
 const server = createServer((req, res) => {
-    // if (req.url === "/") {
-    //     fs.readFile(
-    //         path.join('./', 'public', 'index.html'),
-    //         (err, content) => {
-    //             if (err) throw err;
-    //             res.writeHead(200, { 'Content-Type': 'text/html' });
-    //             res.end(content);
-    //         }
-    //     )
-    // }
-    console.log(req.url);
+
 
     let filePath = path.join(
         './',
